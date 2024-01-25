941b0db3804d3fe1ea3bee9cdf0eb4970385a828
lremor <luisremor@hotmail.com>
2024-01-24 23:21:38 -0300

    exercicio 1 e 2

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,6 @@
 
+import os
+
 ## x = float(input('Digite a coordenada X: \n'))
 ## y = float(input('Digite a coordenada Y: \n'))
 
@@ -35,39 +37,99 @@
 ## else:
 ##     print("Credenciais inválidas. Tente novamente.")
 
+## ---------------------------------------------------------
 
-lista_num = [1,2,3,4,5,6,7,8,9,10]
-lista_nome = ['Luís', 'Liana', 'Gabi', 'Lelo']
-lista_ano = [1990,2024]
-pares = []
-impares = []
+# lista_num = [1,2,3,4,5,6,7,8,9,10]
+# lista_nome = ['Luís', 'Liana', 'Gabi', 'Lelo']
+# lista_ano = [1990,2024]
+# pares = []
+# impares = []
 
-def testetry():
-    try:
-        media = soma_de_todos / len(lista_num)
-        print('A média de todos é:',media)
-    except:
-        print('A lista esta vazia')
+# def testetry():
+#     try:
+#         media = soma_de_todos / len(lista_num)
+#         print('A média de todos é:',media)
+#     except:
+#         print('A lista esta vazia')
 
-for num in lista_num:
-    print(f'{num}')
-    if num % 2 == 0:
-        pares.append(num)
-    else:
-        impares.append(num)
-soma_dos_impares = sum(impares)
-soma_de_todos = sum(impares+pares)
-lista_num.sort(reverse=True)
+# for num in lista_num:
+#     print(f'{num}')
+#     if num % 2 == 0:
+#         pares.append(num)
+#     else:
+#         impares.append(num)
+# soma_dos_impares = sum(impares)
+# soma_de_todos = sum(impares+pares)
+# lista_num.sort(reverse=True)
 
-print(f'{lista_num}')
-print(f'Nomes: {lista_nome}')
-print(f'Lista de pares: {pares}')
-print(f'Lista de ímpares: {impares}')
-print(f'Soma dos ímpares: {soma_dos_impares}')
-print(f'Soma de todos: {soma_de_todos}')
-testetry()
+# print(f'{lista_num}')
+# print(f'Nomes: {lista_nome}')
+# print(f'Lista de pares: {pares}')
+# print(f'Lista de ímpares: {impares}')
+# print(f'Soma dos ímpares: {soma_dos_impares}')
+# print(f'Soma de todos: {soma_de_todos}')
+# testetry()
 
-tabuada = int(input('\nDigite um numero \n\n'))
-for numero in range(11):
-    resultado = tabuada * numero
-    print(f'{tabuada} X {numero} = {resultado}')
+# tabuada = int(input('\nDigite um numero \n\n'))
+# for numero in range(11):
+#     resultado = tabuada * numero
+#     print(f'{tabuada} X {numero} = {resultado}')
+
+# ---------------------------------------------------------
+
+dados_pessoa = [{
+    'nome':'Luís', 
+    'idade':33, 
+    'cidade':'Erechim'
+    }]
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal! ')
+    main()
+
+def subtitulo(texto):
+    linha = '*' * len(texto)
+    print(linha)
+    print(texto)
+    print(linha)
+    print()
+
+def alterar_idade():
+    subtitulo('Alterando a idade')  
+    nome_da_pessoa = input('Digite o nome da pessoa que deseja alterar a idade: \n\n')
+    for pessoa in dados_pessoa:
+        if nome_da_pessoa == pessoa['nome']:
+            idade_pessoa = int(pessoa['idade'])
+            print(f'\nA idade de {nome_da_pessoa} é {idade_pessoa} anos')
+            nova_idade = float(input(f'\nDigite a nova idade de {nome_da_pessoa}: \n\n'))
+            pessoa['idade'] = nova_idade
+            idade_pessoa = int(pessoa['idade'])             
+            print(f'\nA nova idade de {nome_da_pessoa} é {idade_pessoa} anos\n')
+            adicionar_profissao()
+        else:
+            print('\nEsse nome não existe!\n')
+            alterar_idade()
+
+def adicionar_profissao():
+    subtitulo('Adicionando uma profissão')
+    nome_da_pessoa = input('Digite o nome da pessoa que deseja adicionar a profissão: \n\n')
+    for pessoa in dados_pessoa:
+        if nome_da_pessoa == pessoa['nome']:
+            profissao = input(f'\nDigite o nome da profissão de {nome_da_pessoa}: \n\n')
+            pessoa['profissao'] = profissao
+            print(f'\nA profissão {profissao} foi adicionada ao usuário {nome_da_pessoa}')
+            profissao_atualizada = pessoa['profissao']
+            print(f'\nA profissão de {nome_da_pessoa} agora é {profissao_atualizada}')
+            voltar_ao_menu_principal()
+
+        else:
+            print('\nEsse nome não existe!\n')
+            adicionar_profissao()
+        
+
+def main():
+    os.system('cls')
+    alterar_idade()
+
+if __name__ == '__main__':
+    main()
